086d59e3d0a0a71826b58a195c26fa40bf157803
Dmitry Nefedkin <dnefedkin@gmail.com>
2012-11-28 13:20:44 +0400

    Using CSF for storing ADF Application settings - done with layout and bindings

--- StoringConfigurationValues/UsingCSF/ViewController/src/com/blogspot/middletier/view/utils/CSFContextWrapper.java
+++ StoringConfigurationValues/UsingCSF/ViewController/src/com/blogspot/middletier/view/utils/CSFContextWrapper.java
@@ -1,6 +1,7 @@
 package com.blogspot.middletier.view.utils;
 
-import java.util.Map;
+import java.util.*;
+
 
 public class CSFContextWrapper {
     public CSFContextWrapper() {
@@ -12,9 +13,26 @@ public class CSFContextWrapper {
         return res.toString();
     }
     
-    public Map<String, Object> getGenericSettingAsMap (String credMap, String credKey) throws CSFException {
-        Object res =  CSFContext.getInstance().getGenericSetting(credMap, credKey);
-        return  (Map<String, Object>)res;
+
+    
+    public List<Pair> getGenericSettingAsMap (String credMap, String credKey) throws CSFException {
+        List<Pair> result = new ArrayList<Pair>();
+        Object callRes=  CSFContext.getInstance().getGenericSetting(credMap, credKey);
+        Map<String, Object> settings =  (Map<String, Object>)callRes;              
+        for (Map.Entry<String, Object> setting: settings.entrySet()) {
+            // returned value is char[], so combining the string from this char array
+            result.add(new Pair(setting.getKey(), new String((char[])setting.getValue()) ));
+        }
+        return  result;
+    }
+    
+    public List<Pair> getPasswordSetting(String credMap, String credKey) throws CSFException {
+        List<Pair> result = new ArrayList<Pair>();
+        Map<String, String> passSettings = CSFContext.getInstance().getPasswordSetting(credMap, credKey);
+        for (Map.Entry<String,String> setting: passSettings.entrySet()) {
+            result.add(new Pair(setting.getKey(), setting.getValue()));
+        }
+        return result;
     }
 
         
